7c05b766e8081632bca7db7143f35076fef34fbb
sou31415 <tbc3141@icloud.com>
2024-02-15 23:13:05 +0900

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+import { ADDRESS } from './secret';
+import { BaseType, PostType } from './type';
 import "./App.css";
-import { ADDRESS } from "./secret";
-import { BaseType } from "./type";
 const App: React.FC = () => {
-	const [info, setInfo] = useState<Array<BaseType>>([]);
-	//const [name, setName] = useState<String>("");
-	//const [pass, setPass] = useState<String>("");
+	const [user, setUser] = useState<string>('');
+	const [message, setMessage] = useState<string>('');
+	const [messages, setMessages] = useState<Array<BaseType>>([]);
+
 	useEffect(() => {
 		const fetchData = async () => {
-			try {
-				const response = await fetch(ADDRESS, {
-					mode: "cors",
-					method: "GET",
-					headers: { "Content-Type": "application/json" }
-				});
-				const data: Array<BaseType> = await response.json();
-				setInfo(data);
-			} catch (error) {
-				console.error(error);
-			}
+			const response = await fetch(ADDRESS, {
+				mode: "cors",
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+			const data: Array<BaseType> = await response.json();
+			setMessages(data);
 		};
 		fetchData();
 	}, []);
-	const resource = info.map(x =>
-		<div className="container">
-			<p className="box">Account : @{x.user}</p>
-			<p className="box">{x.message}</p>
-		</div>
-	);
-	/*
-	const loginpage = (): JSX.Element => {
-		return (
-			<form></form>
-		)
+
+	const handleSubmit = async () => {
+		const getCurrentUnixTimeBigInt = (): bigint => {
+			const currentUnixTime = Math.floor(Date.now() / 1000);
+			return BigInt(currentUnixTime);
+		};
+		await fetch(ADDRESS + '/register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ user: user, message: message, time: getCurrentUnixTimeBigInt }),
+		});
 	};
-	*/
+
 	return (
-		<>
-			{resource}
-		</>
+		<div>
+			<div>
+				<input type="text" value={user} placeholder="Your Name" onChange={(e) => setUser(e.target.value)} /><br />
+				<input type="text" value={message} placeholder="Your Message" onChange={(e) => setMessage(e.target.value)} />
+				<button onClick={handleSubmit}>Send Message</button>
+			</div>
+			<div>
+				{messages.map(x => (
+					<div className="container">
+						<p className="box">Account : @{x.user}</p>
+						<p className="box">{x.message}</p>
+					</div>
+				))}
+			</div>
+		</div>
 	);
 }
 export default App;
--- src/type.ts
+++ src/type.ts
@@ -2,3 +2,9 @@ export interface BaseType {
     user: string;
     message: string;
 };
+
+export interface PostType {
+    user: string;
+    message: string;
+    time: BigInt;
+}
