a95fce041d33fd89cdb910331372305b4321c176
sou31415 <tbc3141@icloud.com>
2024-02-19 04:32:30 +0900

    サーバーへの負荷高めだが、治った

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { ADDRESS } from './secret';
-import { BaseType, PostType } from './type';
+import { BaseType } from './type';
 import "./App.css";
 const App: React.FC = () => {
 	const [user, setUser] = useState<string>('');
 	const [message, setMessage] = useState<string>('');
 	const [messages, setMessages] = useState<Array<BaseType>>([]);
+	const [pass, setPass] = useState<string>("");
+	const [login, setLogin] = useState<boolean>(false);
 
+	const handleLogin = async () => {
+		const response = await fetch(ADDRESS + '/login/cert', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ user: user, pass: pass }),
+		});
+		const stat = await response.text();
+		if (stat === "Ok") {
+			alert('Succeeded Login!');
+			setLogin(true);
+		} else {
+			alert('Failed Login. Please retry.');
+		}
+	};
+
+	const [resource, setResource] = useState<JSX.Element>(<div><center>
+		<input type="text" placeholder="Enter your name..." onChange={(e) => setUser(e.target.value)} /><br />
+		<input type="password" placeholder="Enter the password..." onChange={(e) => setPass(e.target.value)} /><br />
+		<button onClick={handleLogin}>Login(Register)</button></center>
+	</div>);
+	const Logout = () => {
+		setLogin(false);
+	}
 	useEffect(() => {
 		const fetchData = async () => {
 			const response = await fetch(ADDRESS, {
@@ -17,10 +44,28 @@ const App: React.FC = () => {
 				},
 			});
 			const data: Array<BaseType> = await response.json();
-			setMessages(data);
+			if (login) {
+				setMessages(data.reverse());
+				setResource(<div>
+					<div><button onClick={Logout}>Logout</button>
+						<center>
+							<input type="text" value={message} placeholder="Your Message" onChange={(e) => setMessage(e.target.value)} />
+							<br /><button onClick={handleSubmit}>Send Message</button>
+						</center>
+					</div>
+					<div>
+						{messages.map(x => (
+							<div className="container">
+								<p className="box">Account : @{x.user}</p>
+								<p className="box">{x.message}</p>
+							</div>
+						))}
+					</div>
+				</div>)
+			}
 		};
 		fetchData();
-	}, []);
+	}, [login, messages]);
 
 	const handleSubmit = async () => {
 		const getCurrentUnixTimeBigInt = (): bigint => {
@@ -37,21 +82,9 @@ const App: React.FC = () => {
 	};
 
 	return (
-		<div>
-			<div>
-				<input type="text" value={user} placeholder="Your Name" onChange={(e) => setUser(e.target.value)} /><br />
-				<input type="text" value={message} placeholder="Your Message" onChange={(e) => setMessage(e.target.value)} />
-				<button onClick={handleSubmit}>Send Message</button>
-			</div>
-			<div>
-				{messages.map(x => (
-					<div className="container">
-						<p className="box">Account : @{x.user}</p>
-						<p className="box">{x.message}</p>
-					</div>
-				))}
-			</div>
-		</div>
+		<>
+			{resource}
+		</>
 	);
 }
 export default App;
